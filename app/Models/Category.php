5cc07213920046031c756e1045d40a154d37b387
Aryusniardi <aryusniardi15@gmail.com>
2020-07-31 01:05:00 +0700

    Revert "Revert "kedaifesyen 02""
    This reverts commit 8b11b906fe66228f150104603a0c27b56296e3b1.

---
+++ app/Models/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    //
+    protected $fillable = [
+    	'name','slug','image','status'
+    ];
+    public function books(){
+    	return $this->belongsToMany('App\Models\Fashion','fashion_category','category_id','fashion_id');
+    }
+}
